5f4e918f793688b89dc3d205224978b017c095c5
Damian Blanck <dblanck.is@umbc.edu>
2012-04-07 21:29:29 -0400

    fixing errors

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -6,40 +6,39 @@ render_views
 		before(:each) do
 			@user = Factory(:user)
 		end
-		#it "should be successful" do
-		#	get :show, :id => @user
-		#	response.should be_success
-		#end
-	#it "should find the right user" do
-	#get :show, :id => @user
-	#assigns(:user).should == @user
-	#end
+		it "should be successful" do
+			get :show, :id => @user
+			response.should be_success
+		end
+		it "should find the right user" do
+			assigns(:user).should == @user
+			get :show, :id => @user
+		end
 	
-
-	#it "should have the right title" do
-	#get :show, :id => @user
-	#response.should have_selector("title", :content => @user.name)
-	#end
-	#it "should include the user's name" do
-	#get :show, :id => @user
-	#response.should have_selector("h1", :content => @user.name)
-	#end
-	#it "should have a profile image" do
-	#get :show, :id => @user
-	#response.should have_selector("h1>img", :class => "gravatar")
-	#end
+		it "should have the right title" do
+			get :show, :id => @user
+			response.should have_selector("title", :content => @user.name)
+		end
+		it "should include the user's name" do
+			get :show, :id => @user
+			response.should have_selector("h1", :content => @user.name)
+		end
+		it "should have a profile image" do
+			get :show, :id => @user
+			response.should have_selector("h1>img", :class => "gravatar")
+		end
 	end
 
 
 	describe "GET 'new'" do
 
-	it "should be successful" do
-	get 'new'
+	  it "should be successful" do
+	  get 'new'
 		response.should be_success
-	end
-	it "should have the right title" do
-	get 'new'
-	response.should have_selector("title", :content => "Sign up")
-	end
+	  end
+	  it "should have the right title" do
+	  get 'new'
+	  	response.should have_selector("title", :content => "Sign up")
+	  end
 	end
 end
