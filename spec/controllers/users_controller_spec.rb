fd39f625acdffd87d5ddb399d451081d08174b0d
Damian Blanck <dblanck.is@umbc.edu>
2012-04-09 15:47:03 -0400

    its working now

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -11,8 +11,9 @@ render_views
 			response.should be_success
 		end
 		it "should find the right user" do
-			assigns(:user).should == @user
 			get :show, :id => @user
+			assigns(:user).should == @user
+			
 		end
 	
 		it "should have the right title" do
