8a01fe573781220f8cddc545554679031063b2a7
Damian Blanck <dblanck.is@umbc.edu>
2012-04-22 14:31:43 -0400

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,3 +5,11 @@ user.email  "mhartl@example.com"
 user.password "foobar"
 user.password_confirmation "foobar"
 end
+
+Factory.sequence :name do |n|
+"Person #{n}"
+end
+
+Factory.sequence :email do |n|
+"person-#{n}@example.com"
+end
