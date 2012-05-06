441b93615ffbd38001cf024b75273893f44f34a9
Damian Blanck <dblanck.is@umbc.edu>
2012-05-06 12:29:30 -0400

    12.17

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,11 @@ has_many :microposts, :dependent => :destroy
 has_many :relationships, :foreign_key => "follower_id",
 		:dependent => :destroy
 has_many :following, :through => :relationships, :source => :followed
+has_many :reverse_relationships, :foreign_key => "followed_id",
+:class_name => "Relationship",
+:dependent => :destroy
+has_many :followers, :through => :reverse_relationships, :source => :follower
+
 
 email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -55,11 +60,15 @@ validates :password, :presence => true,
 		relationships.create!(:followed_id => followed.id)
 	end
 
+	def unfollow!(followed)
+		relationships.find_by_followed_id(followed).destroy
+	end
+
 	def feed
 	# This is preliminary. See Chapter 12 for the full implementation.
 		Micropost.where("user_id = ?", id)
 	end
-
+	
 	private
 	   def encrypt_password
 		self.salt = make_salt unless has_password?(password)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -200,6 +200,25 @@ end
 			@user.follow!(@followed)
 			@user.following.should include(@followed)
 		end
+		it "should have an unfollow! method" do
+			@followed.should respond_to(:unfollow!)
+		end
+		it "should unfollow a user" do
+			@user.follow!(@followed)
+			@user.unfollow!(@followed)
+			@user.should_not be_following(@followed)
+		end
+		it "should have a reverse_relationships method" do
+			@user.should respond_to(:reverse_relationships)
+		end
+		it "should have a followers method" do
+			@user.should respond_to(:followers)
+		end
+		it "should include the follower in the followers array" do
+			@user.follow!(@followed)
+			@followed.followers.should include(@user)
+		end
+
 	end
 
 end
