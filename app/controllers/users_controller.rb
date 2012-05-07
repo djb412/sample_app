1808ec90834249f829fdef24f15801a1c559e8b5
Damian Blanck <dblanck.is@umbc.edu>
2012-05-06 20:51:43 -0400

    Listing 12.30

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -63,6 +63,19 @@ class UsersController < ApplicationController
  		redirect_to users_path
 	end
 	
+	def following
+		@title = "Following"
+		@user = User.find(params[:id])
+		@users = @user.following.paginate(:page => params[:page])
+		render 'show_follow'
+	end
+	def followers
+		@title = "Followers"
+		@user = User.find(params[:id])
+		@users = @user.followers.paginate(:page => params[:page])
+		render 'show_follow'
+	end
+
 	private
 		def authenticate
 			deny_access unless signed_in?
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ SampleApp::Application.routes.draw do
 
   	resources :sessions, :only => [:new, :create, :destroy]
 	resources :microposts, :only => [:create, :destroy]
+	resources :relationships, :only => [:create, :destroy]
 
 	match '/signup', :to => 'users#new'
 	match '/signin', :to => 'sessions#new'
